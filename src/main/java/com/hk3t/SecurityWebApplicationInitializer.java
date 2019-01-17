272bab63d9bc38af9f1a04eba8e27250cae337ca
HUNGDT <hk2t.thaihung@gmail.com>
2019-01-17 11:17:05 +0700

    blog

---
+++ src/main/java/com/hk3t/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.hk3t;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
